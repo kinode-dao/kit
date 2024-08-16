aa64115484b409ae85b6af112b6d7a925215e607
hosted-fornet <hosted-fornet@protonmail.com>
2024-08-16 15:59:30 -0700

    new: fix more tests

--- src/new/templates/javascript/no-ui/fibonacci/test/fibonacci_test/fibonacci_test/src/lib.rs
+++ src/new/templates/javascript/no-ui/fibonacci/test/fibonacci_test/fibonacci_test/src/lib.rs
@@ -1,4 +1,4 @@
-use crate::kinode::process::echo::{Request as FibRequest, Response as FibResponse};
+use crate::kinode::process::fibonacci::{Request as FibRequest, Response as FibResponse};
 use crate::kinode::process::tester::{Request as TesterRequest, Response as TesterResponse, RunRequest, FailResponse};
 
 use kinode_process_lib::{await_message, call_init, print_to_terminal, Address, ProcessId, Request, Response};
@@ -7,7 +7,7 @@ mod tester_lib;
 
 wit_bindgen::generate!({
     path: "target/wit",
-    world: "echo-test-template-dot-os-v0",
+    world: "fibonacci-test-template-dot-os-v0",
     generate_unused_types: true,
     additional_derives: [PartialEq, serde::Deserialize, serde::Serialize, process_macros::SerdeJsonInto],
 });
@@ -17,9 +17,9 @@ fn test_number(n: u32, address: &Address) -> anyhow::Result<u64> {
         .target(address)
         .body(FibRequest::Number(n))
         .send_and_await_response(15)?.unwrap();
-    if response.is_request() { fail!("echo_test"); };
+    if response.is_request() { fail!("fibonacci_test"); };
     let FibResponse::Number(fib_number) = response.body().try_into()? else {
-        fail!("echo_test");
+        fail!("fibonacci_test");
     };
     Ok(fib_number)
 }
@@ -29,9 +29,9 @@ fn test_numbers(n: u32, n_trials: u32, address: &Address) -> anyhow::Result<u64>
         .target(address)
         .body(FibRequest::Numbers((n, n_trials)))
         .send_and_await_response(15)?.unwrap();
-    if response.is_request() { fail!("echo_test"); };
+    if response.is_request() { fail!("fibonacci_test"); };
     let FibResponse::Numbers((fib_number, _)) = response.body().try_into()? else {
-        fail!("echo_test");
+        fail!("fibonacci_test");
     };
     Ok(fib_number)
 }
@@ -53,12 +53,12 @@ fn handle_message (our: &Address) -> anyhow::Result<()> {
         input_node_names: node_names,
         ..
     }) = message.body().try_into()?;
-    print_to_terminal(0, "echo_test: a");
+    print_to_terminal(0, "fibonacci_test: a");
     assert!(node_names.len() == 1);
 
     let our_fib_address = Address {
         node: our.node.clone(),
-        process: ProcessId::new(Some("echo"), "echo", "template.os"),
+        process: ProcessId::new(Some("fibonacci"), "fibonacci", "template.os"),
     };
 
     let numbers = vec![0, 1, 2, 5, 10, 20, 30, 47];
@@ -66,7 +66,7 @@ fn handle_message (our: &Address) -> anyhow::Result<()> {
     for (number, expected) in numbers.iter().zip(expecteds.iter()) {
         let result = test_number(number.clone(), &our_fib_address)?;
         if &result != expected {
-            fail!("echo_test");
+            fail!("fibonacci_test");
         }
     }
 
@@ -75,7 +75,7 @@ fn handle_message (our: &Address) -> anyhow::Result<()> {
     for (number, expected) in numbers.iter().zip(expecteds.iter()) {
         let result = test_numbers(number.clone(), 5, &our_fib_address)?;
         if &result != expected {
-            fail!("echo_test");
+            fail!("fibonacci_test");
         }
     }
 
@@ -95,9 +95,9 @@ fn init(our: Address) {
         match handle_message(&our) {
             Ok(()) => {},
             Err(e) => {
-                print_to_terminal(0, format!("echo_test: error: {e:?}").as_str());
+                print_to_terminal(0, format!("fibonacci_test: error: {e:?}").as_str());
 
-                fail!("echo_test");
+                fail!("fibonacci_test");
             },
         };
     }
