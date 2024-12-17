9670a063739b4a681a0f71d5d648f594816d200c
hosted-fornet <hosted-fornet@protonmail.com>
2024-12-17 10:57:29 -0800

    allow building non-simulation mode packages

--- src/boot_fake_node/mod.rs
+++ src/boot_fake_node/mod.rs
@@ -80,14 +80,14 @@ pub fn compile_runtime(path: &Path, release: bool, is_simulation_mode: bool) -> 
     info!("Compiling Kinode runtime...");
 
     // build the packages
-    let args = vec![
+    let mut args = vec![
         "run",
         "-p",
         "build-packages",
-        "--",
-        "--features",
-        "simulation-mode",
     ];
+    if is_simulation_mode {
+        args.extend_from_slice(&["--", "--features", "simulation-mode"]);
+    }
     build::run_command(Command::new("cargo").args(&args).current_dir(path), false)?;
 
     // build the runtime
